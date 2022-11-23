1581463dc1b3655fcfea7954a29704880c3bf740
Stephanie AHossi <ahossi.stephanie@gmail.com>
2022-11-23 14:32:31 +0100

    models

---
+++ models/Project.model.js
@@ -0,0 +1,101 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const projectSchema = new Schema(
+  {
+    number: {
+      type: String,
+      required: [true, "Project number is required."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    title: {
+      type: String,
+      required: [true, "Project title is required."],
+    },
+    country: {
+      type: String,
+      enum: [
+        "AFGHANISTAN",
+        "ALBANIA",
+        "ALGERIA",
+        "ANGOLA",
+        "ANGUILLA",
+        "BRESIL",
+        "BENIN",
+        "IRAN",
+        "NORWAY",
+      ],
+    },
+    geographicalZOne: {
+      type: String,
+      enum: [
+        "Europe",
+        "Middle East",
+        "Asia",
+        "Africa",
+        "South America",
+        "North America",
+      ],
+    },
+    address: {
+      type: String,
+    },
+    activity: {
+      type: String,
+      enum: [
+        "Drinking Water",
+        "Waste Water",
+        "Electricity",
+        "Energy Efficiency",
+        "District Energy",
+        "Waste",
+        "Other Activity",
+      ],
+    },
+    service: {
+      type: String,
+      enum: [
+        "Design and Supervision",
+        "General Studies",
+        "Digital Services",
+        "Asset Management",
+        "Technical and Commercial Losses",
+        "Strategic and Operational Assistance",
+      ],
+    },
+    status: {
+      type: String,
+      enum: [
+        "In preparation",
+        "Submitted",
+        "Awarded",
+        "Signed",
+        "Lost",
+        "Abandoned",
+        "Canceled",
+      ],
+    },
+    creator: {
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+    },
+    contributor: {
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+    },
+    validator: {
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Project = model("User", userSchema);
+
+module.exports = Project;
--- models/User.model.js
+++ models/User.model.js
@@ -3,21 +3,51 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    firstName: {
+      type: String,
+      required: [true, "firstName is required."],
+      unique: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: [true, "LastName is required."],
+      unique: true,
+      trim: true,
+    },
     email: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, "Email is required."],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
-    password: {
+    hashedPassword: {
       type: String,
-      required: [true, 'Password is required.']
-    }
+      required: [true, "Password is required."],
+      validate: {
+        validator: function (v) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email!`,
+      },
+    },
+    image: {
+      type: String,
+      default: "ironhacklogo.png",
+    },
+    role: {
+      type: String,
+      enum: ["user", "manager"],
+    },
+    createdProjects: {
+      type: [Schema.Types.ObjectId],
+      ref: "Project",
+    },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
   }
 );
 
